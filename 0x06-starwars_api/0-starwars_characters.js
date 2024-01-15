d2d24c00fa501c8a12b8ee17f756968b3095bda0
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-15 22:49:18 +0300

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,48 @@
+#!/usr/bin/node
+
+const request = require('request');
+const async = require('async');
+
+const movieId = process.argv[2];
+
+const starWarsUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+async.waterfall([
+  function (callback) {
+    request.get(starWarsUrl, (error, response, body) => {
+      if (error) {
+        callback(error);
+      } else {
+        const result = JSON.parse(body).characters;
+        callback(null, result);
+      }
+    });
+  },
+  function (characters, callback) {
+    async.eachSeries(
+      characters,
+      function (character, innerCallback) {
+        request.get(character, (error, response, body) => {
+          if (error) {
+            innerCallback(error);
+          } else {
+            const personName = JSON.parse(body).name;
+            console.log(personName);
+            innerCallback();
+          }
+        });
+      },
+      function (error) {
+        if (error) {
+          callback(error);
+        } else {
+          callback();
+        }
+      }
+    );
+  }
+], function (error) {
+  if (error) {
+    console.error(error);
+  }
+});
