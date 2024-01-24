cb69a8b0680115608780ac874a716108042fcc14
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-24 08:37:10 +0300

    Refactor code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,49 +1,56 @@
 #!/usr/bin/node
 
 const request = require('request');
-const async = require('async');
 
 const movieId = process.argv[2];
 
 const starWarsUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
+// Function to get characters from the movie
+function getCharacters (callback) {
+  request.get(starWarsUrl, (error, response, body) => {
+    if (error) {
+      callback(error);
+    } else {
+      const result = JSON.parse(body).characters;
+      callback(null, result);
+    }
+  });
+}
 
-async.waterfall([
-  function (callback) {
-    request.get(starWarsUrl, (error, response, body) => {
-      if (error) {
-        callback(error);
-      } else {
-        const result = JSON.parse(body).characters;
-        callback(null, result);
-      }
-    });
-  },
-  function (characters, callback) {
-    async.eachSeries(
-      characters,
-      function (character, innerCallback) {
-        request.get(character, (error, response, body) => {
-          if (error) {
-            innerCallback(error);
-          } else {
-            const personName = JSON.parse(body).name;
-            console.log(personName);
-            innerCallback();
-          }
-        });
-      },
-      function (error) {
-        if (error) {
-          callback(error);
-        } else {
-          callback();
-        }
-      }
-    );
-  }
-], function (error) {
+// Function to process each character
+function processCharacter (character, innerCallback) {
+  request.get(character, (error, response, body) => {
+    if (error) {
+      innerCallback(error);
+    } else {
+      const personName = JSON.parse(body).name;
+      console.log(personName);
+      innerCallback();
+    }
+  });
+}
+
+// Main logic
+getCharacters((error, characters) => {
   if (error) {
     console.error(error);
+    return;
   }
+
+  // Process each character in series
+  let index = 0;
+  function processNextCharacter () {
+    if (index < characters.length) {
+      processCharacter(characters[index], (innerError) => {
+        if (innerError) {
+          console.error(innerError);
+        }
+        index++;
+        processNextCharacter();
+      });
+    }
+  }
+
+  processNextCharacter();
 });
