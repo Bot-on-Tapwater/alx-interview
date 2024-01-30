086f3eaf655488f1b36b7d9455884a98449aa7f8
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-30 15:12:13 +0300

    Refactor code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,43 +1,33 @@
 #!/usr/bin/python3
-"""Implement makeChange() function"""
+"""
+Given a pile of coins of different values, determine the
+fewest number of coins needed to meet a given amount total.
+
+Prototype: def makeChange(coins, total)
+Return: fewest number of coins needed to meet total
+If total is 0 or less, return 0
+If total cannot be met by any number of coins you have, return -1
+coins is a list of the values of the coins in your possession
+The value of a coin will always be an integer greater than 0
+You can assume you have an infinite number of each
+denomination of coin in the list
+Your solution’s runtime will be evaluated in this task
+
+"""
 
 
 def makeChange(coins, total):
-    """
-    Calculates the minimum number of coins
-    needed to make change for a given total amount.
-
-    Args:
-        coins (List[int]): The denominations of the available coins.
-        total (int): The total amount for which to make change.
-
-    Returns:
-        int: The minimum number of coins needed to make change for the total
-        amount. Returns -1 if it's not possible to make change.
-    """
-
     if total <= 0:
         return 0
+    count = 0
+    lg_idx = 0
+    coins.sort(reverse=True)
 
-    """
-    Initialize an array to store the minimum
-    number of coins needed for each amount
-    """
-    dp = [float('inf')] * (total + 1)
-
-    # There are 0 coins needed to make change for amount 0
-    dp[0] = 0
-
-    '''
-    Iterate through each coin and update the
-    minimum number of coins for each amount
-    '''
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    """
-    If the value at the total amount is still infinity,
-    it means it's not possible to make change
-    """
-    return dp[total] if dp[total] != float('inf') else -1
+    while total > 0 and lg_idx < len(coins):
+        largest = coins[lg_idx]
+        count += total // largest
+        total %= largest
+        lg_idx += 1
+    if total != 0:
+        return -1
+    return count
